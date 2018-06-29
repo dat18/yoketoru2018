5a8c0a33c9562b69bd5bb29bf67733bedb361156
tanakaedu <tanaka@am1.jp>
2018-06-29 16:11:25 +0900

    GameSystemをシングルトン化
    TODO: 各シーンの開始時に、スコアのテキストのインスタンスを,
    GameSystem.Instance.SetScoreText()で渡す処理を追加

---
+++ Assets/GameSystem.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameSystem : Singleton<GameSystem> {
+
+    [SerializeField]
+    TextMeshProUGUI scoreText;
+
+    int score;
+
+    // Use this for initialization
+    void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+        // Aキーが押されたら点数を入れる
+        if (Input.GetKey(KeyCode.A))
+        {
+            score += 10;
+            updateScore();
+        }
+    }
+
+    public void SetScoreText(TextMeshProUGUI txt)
+    {
+        scoreText = txt;
+        updateScore();
+    }
+
+    void updateScore()
+    {
+        scoreText.text = score.ToString("D7");
+    }
+}

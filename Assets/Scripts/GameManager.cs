c20008990cd27ec034377093067f009ad2250daa
tanakaedu <tanaka@am1.jp>
2018-06-29 14:55:34 +0900

    タイトル-ゲーム-ゲームオーバーの遷移を実装

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+        // Oキーが押されたか判定
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            // マルチシーンで、GameOverシーンを読み込む方法
+            SceneManager.LoadScene("GameOver", LoadSceneMode.Additive);
+        }
+	}
+}
---
+++ Assets/Scripts/GameOverManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+        // クリックされたか？
+        if (Input.GetMouseButtonDown(0))
+        {
+            // Titleシーンを読み込む
+            SceneManager.LoadScene("Title");
+        }
+    }
+}
